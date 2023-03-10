e1a887d73a45a93583deee3c26f88351240b9f4b
Ruzell03 <ruzell@matchmo.ph>
2023-03-10 08:37:56 +0800

    implement middleware

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,35 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { QueryClientProvider, QueryClient } from "react-query";
+import jsCookie from "js-cookie";
+import middleware from "./middleware";
+import { useEffect } from "react";
+import { NextResponse } from "next/server";
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
+  const router = useRouter();
+  const cookie = jsCookie.get("access-token");
+  const url = router.pathname;
+
+  const handleMiddleware = async () => {
+    const response = await middleware({ cookie, url });
+    if (response instanceof NextResponse) {
+      const location = response.headers.get("Location");
+      if (location) {
+        router.replace(location);
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleMiddleware();
+  }, [router]);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </QueryClientProvider>
   );
 }
